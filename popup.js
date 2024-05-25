9b978c11929c481b2068a9229128058336d981be
sd535682 <sd535682@gmail.com>
2024-05-26 01:54:52 +0530

    Chrome Extension Beta

--- popup.js
+++ popup.js
@@ -1,14 +1,69 @@
-document.addEventListener("DOMContentLoaded", () => {
-  navigator.geolocation.getCurrentPosition(
-    ({ coords: { latitude, longitude } }) => {
-      fetch(`https://wttr.in/${latitude},${longitude}?2Fn`)
-        .then((response) => response.text())
-        .then((data) => {
-          document.getElementById("info").innerHTML = data;
-        })
-        .catch((error) => {
-          document.getElementById("info").innerHTML = `Got an error: ${error}`;
-        });
-    }
-  );
+// navigator.geolocation.getCurrentPosition(function getLocation(location) {
+//   const lat = location.coords.latitude.toFixed(3);
+//   const lon = location.coords.longitude.toFixed(3);
+//   const cityName = getCity(lat, lon);
+// });
+
+// function getCity(lat, lon) {
+//   fetch(
+//     `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}`
+//   )
+//     .then((res) => res.json())
+//     .then((data) => {
+//       if (data.address.city) {
+//         const cityName = data.address.city;
+//       } else if (data.address.county) {
+//         const cityName = data.address.county;
+//       } else if (data.address.state) {
+//         const cityName = data.address.state;
+//       } else {
+//         const cityName = data.address.country;
+//       }
+//       const weather = getWeather(cityName);
+//     });
+// }
+
+// function getWeather(cityName) {
+//   fetch(`https://wttr.in/${cityName}?2Fnq`)
+//     .then((res) => res.text())
+//     .then((data) => {
+//       document.getElementById("info").innerHTML = data;
+//     });
+// }
+
+navigator.geolocation.getCurrentPosition(function getLocation(location) {
+  const lat = location.coords.latitude.toFixed(3);
+  const lon = location.coords.longitude.toFixed(3);
+  getCity(lat, lon);
 });
+
+function getCity(lat, lon) {
+  fetch(
+    `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      let cityName;
+      if (data.address.city) {
+        cityName = data.address.city;
+      } else if (data.address.county) {
+        cityName = data.address.county;
+      } else if (data.address.state) {
+        cityName = data.address.state;
+      } else {
+        cityName = data.address.country;
+      }
+      getWeather(cityName);
+    })
+    .catch((error) => {
+      console.error("Error fetching city:", error);
+    });
+}
+
+function getWeather(cityName) {
+  fetch(`https://wttr.in/${cityName}?2Fnq`)
+    .then((res) => res.text())
+    .then((data) => {
+      document.getElementById("info").innerHTML = data;
+    });
+}
