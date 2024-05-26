55920db20ad510b99fafb7e1e644889c3256ebe0
sd535682 <sd535682@gmail.com>
2024-05-26 18:28:44 +0530

    Chrome Extension Release

--- popup.js
+++ popup.js
@@ -1,36 +1,3 @@
-// navigator.geolocation.getCurrentPosition(function getLocation(location) {
-//   const lat = location.coords.latitude.toFixed(3);
-//   const lon = location.coords.longitude.toFixed(3);
-//   const cityName = getCity(lat, lon);
-// });
-
-// function getCity(lat, lon) {
-//   fetch(
-//     `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}`
-//   )
-//     .then((res) => res.json())
-//     .then((data) => {
-//       if (data.address.city) {
-//         const cityName = data.address.city;
-//       } else if (data.address.county) {
-//         const cityName = data.address.county;
-//       } else if (data.address.state) {
-//         const cityName = data.address.state;
-//       } else {
-//         const cityName = data.address.country;
-//       }
-//       const weather = getWeather(cityName);
-//     });
-// }
-
-// function getWeather(cityName) {
-//   fetch(`https://wttr.in/${cityName}?2Fnq`)
-//     .then((res) => res.text())
-//     .then((data) => {
-//       document.getElementById("info").innerHTML = data;
-//     });
-// }
-
 navigator.geolocation.getCurrentPosition(function getLocation(location) {
   const lat = location.coords.latitude.toFixed(3);
   const lon = location.coords.longitude.toFixed(3);
@@ -65,5 +32,9 @@ function getWeather(cityName) {
     .then((res) => res.text())
     .then((data) => {
       document.getElementById("info").innerHTML = data;
+    })
+    .catch((error) => {
+      document.getElementById("info").innerHTML =
+        "500 Internal Server Error" + error;
     });
 }
